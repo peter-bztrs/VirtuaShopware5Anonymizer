35c3552d3b528bce6a0186d3e160b2463128fc14
kuba <intern4@wearevirtua.com>
2019-05-21 16:33:25 +0200

    ugly but working good autoComposerInstall, note when code moved to object some extra issues appeared

--- VirtuaShopwareAnonymizer.php
+++ VirtuaShopwareAnonymizer.php
@@ -3,12 +3,23 @@
 namespace VirtuaShopwareAnonymizer;
 
 use Shopware\Components\Plugin;
+use Shopware\Components\Plugin\Context\ActivateContext;
+use Shopware\Components\Plugin\Context\InstallContext;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
  * Shopware-Plugin VirtuaShopwareAnonymizer.
  */
 class VirtuaShopwareAnonymizer extends Plugin
 {
+    /**
+     * Build with composer install
+     */
+    public function build(ContainerBuilder $container)
+    {
+        require_once __DIR__ . '/autoComposerInstall.php';
+        parent::build($container);
+    }
 
     /**
      * {@inheritdoc}
@@ -17,78 +28,6 @@ class VirtuaShopwareAnonymizer extends Plugin
     {
         return [
             'Shopware_Console_Add_Command' => 'registerVendor',
-            'Enlight_Bootstrap_AfterInitResource_hooks' => 'composerAutoInstall'
         ];
-    }
-
-    public function composerAutoInstall()
-    {
-        $shopwareRoot = dirname(dirname(dirname(__DIR__)));
-        $extractedComposer = __DIR__ . '/tmp';
-
-        if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
-            $this->extractComposer($extractedComposer, $shopwareRoot);
-            require_once $extractedComposer . '/vendor/autoload.php';
-
-            $this->composerInstall($extractedComposer);
-
-            $this->deleteAll($extractedComposer);
-            $this->deleteAll(__DIR__ . '/vendor/composer');
-        }
-    }
-
-    /**
-     * @param \Enlight_Event_EventArgs $args
-     */
-    public function registerVendor()
-    {
-        if (file_exists($this->getPath() . '/vendor/autoload.php')) {
-            require_once $this->getPath() . '/vendor/autoload.php';
-        }
-    }
-
-    /**
-     * @param $extractedComposer
-     * @param $shopwareRoot
-     */
-    protected function extractComposer($extractedComposer, $shopwareRoot)
-    {
-        if (!file_exists($extractedComposer . '/vendor/autoload.php')) {
-            $composerPhar = new \Phar($shopwareRoot . '/composer.phar');
-            $composerPhar->extractTo($extractedComposer);
-        }
-    }
-
-    /**
-     * @param $extractedComposer
-     */
-    protected function composerInstall($extractedComposer)
-    {
-        putenv('COMPOSER_HOME=' . $extractedComposer . '/bin/composer');
-        chdir(__DIR__);
-        $stream = fopen('php://temp', 'w+');
-        $output = new \Symfony\Component\Console\Output\StreamOutput($stream);
-        $application = new \Composer\Console\Application();
-        $application->setAutoExit(false);
-        $application->run(
-            new \Symfony\Component\Console\Input\ArrayInput(
-                array('command' => 'install')
-            ),
-            $output
-        );
-        fclose($stream);
-    }
-
-    protected function deleteAll($str)
-    {
-        if (is_file($str)) {
-            return unlink($str);
-        } elseif (is_dir($str)) {
-            $scan = glob(rtrim($str, '/') . '/*');
-            foreach ($scan as $path) {
-                deleteAll($path);
-            }
-            return @rmdir($str);
-        }
     }
 }
---
+++ autoComposerInstall.php
@@ -0,0 +1,37 @@
+<?php
+
+if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
+
+    $shopwareRoot = dirname(dirname(dirname(__DIR__)));
+    $extractedComposer = $shopwareRoot . '/tmp';
+
+    if (!file_exists($extractedComposer . '/vendor/autoload.php')) {
+        $composerPhar = new \Phar($shopwareRoot . '/composer.phar');
+        $composerPhar->extractTo($extractedComposer);
+    }
+    require_once $extractedComposer . '/vendor/autoload.php';
+
+    putenv('COMPOSER_HOME=' . $extractedComposer . '/bin/composer');
+    chdir(__DIR__);
+    $stream = fopen('php://temp', 'w+');
+    $output = new \Symfony\Component\Console\Output\StreamOutput($stream);
+    $application = new \Composer\Console\Application();
+    $application->setAutoExit(false);
+    $application->run(new \Symfony\Component\Console\Input\ArrayInput(array('command' => 'install')), $output);
+
+
+    $deleteAll = function ($str) {
+        if (is_file($str)) {
+            return unlink($str);
+        } elseif (is_dir($str)) {
+            $scan = glob(rtrim($str, '/') . '/*');
+            foreach ($scan as $path) {
+                deleteAll($path);
+            }
+            return @rmdir($str);
+        }
+    };
+
+    $deleteAll($extractedComposer);
+    fclose($stream);
+}
