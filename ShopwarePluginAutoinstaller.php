a0d1579815d03ea2fde8c1578019b28d5fbd2810
kuba <intern4@wearevirtua.com>
2019-05-24 13:14:07 +0200

    function installing composer changed

--- ShopwarePluginAutoinstaller.php
+++ ShopwarePluginAutoinstaller.php
@@ -46,10 +46,11 @@ class ShopwarePluginAutoinstaller
             chdir($shopwareComposer);
             shell_exec(
                 <<<EOT
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
+echo "$(curl -sS https://composer.github.io/installer.sig) -" > composer-setup.php.sig
+curl -sS https://getcomposer.org/installer | tee composer-setup.php | sha384sum -c composer-setup.php.sig 
+php composer-setup.php --filename=composer.phar
 php -r "unlink('composer-setup.php');"
+php -r "unlink('composer-setup.php.sig');"
 EOT
             );
         }
