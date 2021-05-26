ef1ce4ca958ab2dd9483201eeded19c8eaa90fc9
Peter Meijer <36953693+peter-bztrs@users.noreply.github.com>
2021-05-26 08:45:47 +0200

    Update CoreAuth.php
    Disabled core auth anominyze

--- Anonymizer/Bridge/Entity/CoreAuth.php
+++ Anonymizer/Bridge/Entity/CoreAuth.php
@@ -5,19 +5,19 @@ namespace VirtuaShopwareAnonymizer\Anonymizer\Bridge\Entity;
 class CoreAuth extends AbstractBridgeEntity
 {
     /** {@inheritdoc} */
-    protected $formattersByAttribute = [
-        'name' => 'name',
-        'email' => 'safeEmail',
-    ];
+    //protected $formattersByAttribute = [
+    //    'name' => 'name',
+    //    'email' => 'safeEmail',
+    //];
 
     /** {@inheritdoc} */
-    protected $uniqueAttributes = [
-        'email'
-    ];
+    //protected $uniqueAttributes = [
+   //     'email'
+    //];
 
     /** {@inheritdoc} */
-    protected $tableName = 's_core_auth';
+    //protected $tableName = 's_core_auth';
 
     /** {@inheritdoc} */
-    protected $entityName = 'Core Auth';
+    //protected $entityName = 'Core Auth';
 }
